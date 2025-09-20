9b11fc6fd8af1d50474d9283f5398b1f477cfb3c
Dream-Link <kavitacloud7@gmail.com>
2025-09-20 11:36:40 +0530

    Add files via upload

---
+++ install.js
@@ -0,0 +1,10 @@
+// Register the service worker to make the app installable
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, err => {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
