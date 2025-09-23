5f1f1905aeb37cd332fea2a21fee2d01eda57e88
Dream-Link <kavitacloud7@gmail.com>
2025-09-23 10:48:11 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,48 @@
-console.log('Hello!');
+const CACHE_NAME = 'study-planner-pro-v1';
+// Files to cache for offline use
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
